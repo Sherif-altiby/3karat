60308c364509f8ad64e00e9e36ba122126b55fc2
elbadry <elbadry.egypt010@gmail.com>
2024-09-24 13:10:52 +0300

    first test

--- js/custom.js
+++ js/custom.js
@@ -90,7 +90,8 @@ if ($("#slider-client").length) {
     margin: 10,
     dots: true,
     autoplay: true,   
-    autoplayTimeout: 3000, 
+    autoplayTimeout: 4000, 
+    smartSpeed:2000,
     autoplayHoverPause: true,
     responsive: {
       0: {
@@ -450,4 +451,70 @@ if(toggleMenuBtn){
   toggleMenuBtn.addEventListener("click", (e) => {
     notificationsMenu.classList.toggle('show')
   })
+}
+
+
+
+//custom 2 
+
+
+
+
+
+
+
+const inputs = document.querySelectorAll('.code-input');
+
+if(inputs){
+    inputs.forEach((input, index) => {
+        input.addEventListener('input', function() {
+            if (input.value.length === 1 && index < inputs.length - 1) {
+                inputs[index + 1].focus(); // Focus on the next input
+            }
+        });
+
+        input.addEventListener('keydown', function(event) {
+            if (event.key === 'Backspace' && input.value.length === 0 && index > 0) {
+                inputs[index - 1].focus(); // Focus on the previous input when backspacing
+            }
+        });
+    });
+}
+
+
+// owl carousel for nav property card
+
+
+
+// amount and precentage
+
+discountType = document.getElementById("discount-type");
+percentage = document.querySelector(".percentage");
+amount = document.querySelector(".amount");
+if(discountType){
+    discountType.addEventListener("change",()=>{
+        if(discountType.value =="percentage"){
+            percentage.style = "display:block"
+            amount.style = "display:none";
+        }
+        else{
+            amount.style = "display:block"
+            percentage.style = "display:none";
+        }
+    })
+}
+
+
+
+
+//  date input
+const dateInput = document.getElementById("dateInput");
+const calendarIcon = document.getElementById("calendarIcon");
+
+if(dateInput){
+calendarIcon?.addEventListener("click", function () {
+    dateInput.showPicker();  
+    console.log(',c,bv')
+  });
+  
 }
